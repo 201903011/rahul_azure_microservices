4602ae9ef7846ce551fa47029a7463198a1e353e
rahulBioFuel <jsd1@biofuelsjunction.com>
2025-03-05 15:29:50 +0530

    web app changes

--- w2/server.js
+++ w2/server.js
@@ -9,11 +9,11 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World from W2 server');
+  res.send('Hello World from web server');
 });
 
 app.get('/getUser', (req, res) => {
-  res.send('getUser Api from W2 server');
+  res.send('getUser Api from web server');
 });
 
 app.listen(PORT, HOST);
